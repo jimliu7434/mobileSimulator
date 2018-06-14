3c110330d8ff09d189e002aedefce6af828d5361
jimliu7434 <jimliu7434@gmail.com>
2018-06-14 18:40:06 +0800

    add element-ui

--- mobile/src/App.vue
+++ mobile/src/App.vue
@@ -4,6 +4,17 @@
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
+    <el-menu
+            :default-active="activeIndex2"
+            class="el-menu-demo"
+            mode="horizontal"
+            @select="handleSelect"
+            background-color="#545c64"
+            text-color="#fff"
+            active-text-color="#ffd04b">
+      <el-menu-item index="1">Processing Center</el-menu-item>
+    </el-menu>
+
     <router-view/>
   </div>
 </template>
